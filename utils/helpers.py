5d2fe6d609089fc6c81c3864e17e329ae93fb0cc
Mel Ugaddan <ugaddanmeljohn@gmail.com>
2026-01-13 19:22:19 +0800

    Added Helper Files

--- utils/helpers.py
+++ utils/helpers.py
@@ -7,6 +7,7 @@ from sqlalchemy.engine import Engine
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 import orjson
+from utils.constants import REDIS_TTL
 
 serializer_fn   = orjson.dumps
 deserializer_fn = orjson.loads
@@ -21,8 +22,7 @@ def generate_redis_cache(engine : Engine, cache_redis : redis.Redis) -> None:
                 "name": user.name,
                 "age": user.age
             }
-            cache_redis.set(cache_key, orjson.dumps(data), ex=120)
-  
+            cache_redis.set(cache_key, orjson.dumps(data), ex=REDIS_TTL)
 
 async def generate_redis_cache_async(engine : Engine, cache_redis : redis.Redis) -> None:
     async with AsyncSession(engine) as session:
@@ -38,8 +38,24 @@ async def generate_redis_cache_async(engine : Engine, cache_redis : redis.Redis)
                     {"id": post.id, "title": post.title, "text": post.text } for post in user.posts
                 ]
             }
-            cache_redis.set(cache_key, orjson.dumps(data), ex=120)
+            cache_redis.set(cache_key, orjson.dumps(data), ex=REDIS_TTL)
 
+async def generate_aredis_cache_async(engine : Engine, cache_redis : redis.asyncio.Redis) -> None:
+    async with AsyncSession(engine) as session:
+        result = await session.execute(select(User))
+        users = result.scalars().all()
+        for user in users:
+            cache_key = f"user-{user.id}"
+            data = {
+                "id": user.id,
+                "name": user.name,
+                "age": user.age,
+                "posts": [ 
+                    {"id": post.id, "title": post.title, "text": post.text } for post in user.posts
+                ]
+            }
+            await cache_redis.set(cache_key, orjson.dumps(data), ex=REDIS_TTL)
+            
 def generate_inprocess_cache(engine : Engine, cache_inprocess : TTLCache ) -> None:
     with Session(engine) as session:
         users = session.query(User)
